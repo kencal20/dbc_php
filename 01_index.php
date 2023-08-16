aca8489d746fecb07ef08295146737e164a6aec1
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-16 11:58:56 +0000

    first /initial php file

---
+++ 01_index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Displaying php</title>
+</head>
+<body>
+    
+<?php 
+echo 'Welcome to DBC29 <br/>';
+
+print(' Hello Kenneth Adjetey');
+
+//print_r(); for printing array
+echo '<br/>';
+var_dump('aaaa');
+?>
+</body>
+</html>
