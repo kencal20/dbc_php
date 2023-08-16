94df1093066df5fd050794d2e500be92b2ff36f0
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-16 12:17:39 +0000

    elevy function creation

--- 08_functions.php
+++ 08_functions.php
@@ -2,14 +2,26 @@
 
 function addnumbers()
 {
-    echo $total = 2 + 3 ,'<br/>'; 
+    echo $total = 2 + 3, '<br/>';
 }
 
 function register($name, $dob)
 {
-    echo 'Student Name is ', $name,'  ';
+    echo 'Student Name is ', $name, '  ';
     echo 'Date of birth is ', $dob;
 }
+
+function calElevy($amt)
+{
+    $eLevyTax = 1 / 100;
+    $amtCharged = $eLevyTax * $amt;
+
+    return $amtCharged;
+}
+
+
 // function call
 addnumbers();
 register('Ama', '04-02-2023');
+
+echo '<br/>','Elevy = ', calElevy(200);
