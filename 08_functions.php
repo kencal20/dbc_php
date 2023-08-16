04c204c5f2c6f8bd664b921eab3f6dac79a5bdfa
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2023-08-16 12:06:55 +0000

    functions creation and function call

--- 08_functions.php
+++ 08_functions.php
@@ -1,6 +1,15 @@
 <?php
 
+function addnumbers()
+{
+    echo $total = 2 + 3;
+}
 
-
-
-?>
+function register($name, $dob)
+{
+    echo 'Student Name is ', $name;
+    echo 'Date of birth is', $dob;
+}
+// function call
+addnumbers();
+register('Ama', '04-02-2023');
